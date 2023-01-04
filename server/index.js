7f65cd26e5fe282ef5d5e9797d593b45f0058de2
Mochamad Rizki <moch.rizki@elnusa.co.id>
2023-01-04 20:30:09 +0700

    scaffold mongodb models

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const schema = require("../schema/schema");
 require("dotenv").config();
-
+const colors = require("colors");
 const { graphqlHTTP } = require("express-graphql");
-
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+connectDB();
 
 app.use(
   "/graphql",
